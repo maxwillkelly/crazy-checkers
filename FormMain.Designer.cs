a851258c26e56aed51f1d8c4f30d2bf9c722af1d
Max Kelly <maxwill.kelly@gmail.com>
2020-02-03 00:53:50 +0000

    Grid created but wonky formatting
    Added TableLayoutPanel grid, added button event handler, deleted example button made in Designer

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿namespace Crazy_Checkers
 {
     partial class FormMain
-    {
+    {   
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // FormMain
@@ -36,7 +35,6 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "FormMain";
             this.Text = "Crazy Checkers";
             this.ResumeLayout(false);
@@ -44,8 +42,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -14,6 +14,7 @@ namespace Crazy_Checkers
     {
         public FormMain()
         {
+            // SuspendLayout();
             InitializeComponent();
             CreateBtnGrid();
         }
@@ -22,20 +23,39 @@ namespace Crazy_Checkers
         {
             uint colSize = 8;
             uint rowSize = 8;
+            uint colLength = 50;
+            uint rowLength = 50;
 
             Button[,] BtnArray = new Button[colSize, rowSize];
+            TableLayoutPanel grid = new TableLayoutPanel();
+            // Adds Grid to Main form
+            Controls.Add(grid);
+            // grid.SuspendLayout();
+            grid.Anchor = (((AnchorStyles.Top | AnchorStyles.Bottom) | AnchorStyles.Left) | AnchorStyles.Right);
+            grid.Dock = DockStyle.Fill;
+            grid.ColumnCount = Convert.ToInt32(colSize);
+            grid.RowCount = Convert.ToInt32(rowSize);
 
             for (uint col = 0; col < colSize; col++)
             {
                 for (uint row = 0; row < rowSize; row++)
                 {
+                    // Places button in array
                     Button btn = BtnArray[col, row];
+                    // Creates button
                     btn = new Button();
-                    btn.Location = new Point(343, 139);
+                    // Adds button to Main form
+                    grid.Controls.Add(btn, Convert.ToInt32(col), Convert.ToInt32(row));
+                    // Calculates and sets button location
+                    // int colLocation = Convert.ToInt32(colLength * col);
+                    // int rowLocation = Convert.ToInt32(rowLength * row);
+                    // btn.Location = new Point(colLocation, rowLocation);
+                    // Names button based on location
                     btn.Name = "Btn_" + col + "_" + row;
-                    btn.Size = new Size(75, 23);
+                    btn.Anchor = AnchorStyles.Right;
+                    btn.AutoSize = true;
+                    btn.Size = new Size(Convert.ToInt32(colLength), Convert.ToInt32(rowLength));
                     btn.TabIndex = 0;
-                    btn.Text = "button1";
                     btn.UseVisualStyleBackColor = true;
                     btn.Click += new EventHandler(BtnClick);
                 }
@@ -43,6 +63,12 @@ namespace Crazy_Checkers
         }
 
         private void BtnClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Console.WriteLine("{0}\n", button.Name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
