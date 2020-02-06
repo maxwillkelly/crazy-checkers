05abd569bed1b154467d21f6a2dc8f4e32683a93
Ramsay Sewell <sewellrf@gmail.com>
2020-02-06 16:52:13 +0000

    uploaded UML output

---
+++ CrazyCheckers/Grid.cs
@@ -0,0 +1,24 @@
+using System;
+namespace Crazy_Checkers {
+	public class Grid {
+		private uint colSize = 8;
+		private uint rowSize = 8;
+		private Position[] positionArray;
+
+		public Grid() {
+			throw new System.Exception("Not implemented");
+		}
+		private void CreateBtnGrid() {
+			throw new System.Exception("Not implemented");
+		}
+		private void BtnClick(ref object sender, ref EventArgs e) {
+			throw new System.Exception("Not implemented");
+		}
+
+		private Game game;
+
+		private Position position;
+
+	}
+
+}
---
+++ CrazyCheckers/Program.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Crazy_Checkers {
+	public class Program {
+		public void Main() {
+			throw new System.Exception("Not implemented");
+		}
+
+		private FormMenu formMenu;
+
+	}
+
+}
