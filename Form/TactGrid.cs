16ee0bc759e0db496bece9143423052f8c437672
Max Kelly <maxwill.kelly@gmail.com>
2020-02-08 21:04:55 +0000

    Sets up base TactGrid class

--- Form/TactGrid.cs
+++ Form/TactGrid.cs
@@ -8,9 +8,30 @@ namespace Crazy_Checkers
 {
     class TactGrid
     {
+        private uint Columns = 8;
+        private uint Rows = 8;
+        private bool[,] validMove;
+
         TactGrid()
         {
+            validMove = new bool[Columns, Rows];
+        }
 
+        TactGrid(uint Columns, uint Rows)
+        {
+            this.Columns = Columns;
+            this.Rows = Rows;
+            validMove = new bool[Columns, Rows];
+        }
+
+        public bool getSquare(uint col, uint row)
+        {
+            return validMove[col, row];
+        }
+
+        public void setSquare(uint col, uint row, bool valid)
+        {
+            validMove[col,row] = valid;
         }
     }
 }
