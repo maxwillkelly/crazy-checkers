cc28013c4031243fd092e2c0e0293b3a4d823ae3
Max Kelly <maxwill.kelly@gmail.com>
2020-02-11 17:13:28 +0000

    Turn Indicators work!!!

--- Form/Game.cs
+++ Form/Game.cs
@@ -69,6 +69,9 @@ namespace Crazy_Checkers
             {
                 CurrentPlayer = players[0];
             }
+            EventArgs e = new EventArgs();
+            //ScoreEventHandler(players, e);
+            TurnChangeEventHandler(CurrentPlayer.playerNum, e);
         }
 
         // Runs when a position is clicked in the grid
